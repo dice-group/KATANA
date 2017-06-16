31d11b7cf25aacebb761084a08a47e361ea5a174
Kunal-Jha <kunal94jha@gmail.com>
2017-06-16 11:54:16 +0200

    Added code to get Functional Properties

--- src/main/java/org/aksw/simba/katana/KBUtils/SparqlHandler.java
+++ src/main/java/org/aksw/simba/katana/KBUtils/SparqlHandler.java
@@ -1,4 +1,4 @@
-package org.aksw.simba.KB;
+package org.aksw.simba.katana.KBUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,17 +8,18 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
-public class KBCBDBuilder {
+public class SparqlHandler {
 
 	private String endpoint = "http://dbpedia.org/sparql";
 	private String graph = "http://dbpedia.org";
 
-	public List<Resource> getNResources(String classname) {
+	public List<Resource> getResources(String classname) {
 		List<Resource> results = new ArrayList<Resource>();
 		String sparqlQueryString = "SELECT DISTINCT ?s { ?s a <" + classname + "> }";
 		QueryFactory.create(sparqlQueryString);
@@ -43,17 +44,28 @@ public class KBCBDBuilder {
 		return cbd;
 	}
 
+	public ResultSet getFunctionalProperties() {
+		List<Resource> results = new ArrayList<Resource>();
+		String sparqlQueryString = "select distinct ?p where {?p a <http://www.w3.org/2002/07/owl#FunctionalProperty>}";
+		QueryFactory.create(sparqlQueryString);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(endpoint, sparqlQueryString, graph);
+		ResultSet cbds = qexec.execSelect();
+		return cbds;
+
+	}
+
 	public static void main(String[] args) {
 		String classname = "http://dbpedia.org/ontology/City";
-		KBCBDBuilder c = new KBCBDBuilder();
+		SparqlHandler c = new SparqlHandler();
 		Model m = ModelFactory.createDefaultModel();
-		List<Resource> x = c.getNResources(classname);
-
+		List<Resource> x = c.getResources(classname);
+		ResultSet a = c.getFunctionalProperties();
+		ResultSetFormatter.outputAsJSON(System.out, a);
 		m.add(c.getCBD(x.get(0)));
 		/*
 		 * for (Resource r : c.getNResources(classname)) { m.add(c.getCBD(r)); }
 		 */
-		m.write(System.out, "TTL");
+		// m.write(System.out, "TTL");
 
 	}
 
